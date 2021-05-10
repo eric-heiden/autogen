09e42bad951ca489e15a24ea9c31ea21f296a44d
Eric Heiden <eric-heiden@users.noreply.github.com>
2021-05-10 00:25:59 -0700

    WIP Python front-end with codegen support

--- python/autogen/__init__.py
+++ python/autogen/__init__.py
@@ -1,12 +1,137 @@
+from typing import Callable
 from _autogen import *
+from enum import Enum
+
+# keeps track of the current autogen mode and other meta information
+__AUTOGEN_SCOPE__ = None
 
 
-def trace(fun, xs) -> CppADFunction:
-    ad_x = CppADVector([CppADScalar(x) for x in xs])
+class Mode(Enum):
+    NUMERICAL = 1
+    CPPAD = 2
+    CPU = 3
+    GPU = 4
+
+
+class Scope:
+    def __init__(self, mode: Mode) -> None:
+        self.mode = mode
+
+
+def scalar_type():
+    global __AUTOGEN_SCOPE__
+    if __AUTOGEN_SCOPE__ is None or __AUTOGEN_SCOPE__.mode == Mode.NUMERICAL:
+        return float
+    if __AUTOGEN_SCOPE__.mode == Mode.CPPAD:
+        return CppADScalar
+    return CGScalar
+
+
+def scalar(x):
+    global __AUTOGEN_SCOPE__
+    if __AUTOGEN_SCOPE__ is None or __AUTOGEN_SCOPE__.mode == Mode.NUMERICAL:
+        return float(x)
+    if __AUTOGEN_SCOPE__.mode == Mode.CPPAD:
+        return CppADScalar(x)
+    return CGScalar(x)
+
+
+def vector_type():
+    global __AUTOGEN_SCOPE__
+    if __AUTOGEN_SCOPE__ is None or __AUTOGEN_SCOPE__.mode == Mode.NUMERICAL:
+        return list
+    if __AUTOGEN_SCOPE__.mode == Mode.CPPAD:
+        return CppADVector
+    return CGVector
+
+
+def trace(fun, xs, mode: Mode = Mode.CPPAD):
+    global __AUTOGEN_SCOPE__
+    if __AUTOGEN_SCOPE__ is None:
+        __AUTOGEN_SCOPE__ = Scope(mode)
+    else:
+        __AUTOGEN_SCOPE__.mode = mode
+
+    Scalar = scalar_type()
+    Vector = vector_type()
+
+    ad_x = Vector([Scalar(x) for x in xs])
+
     independent(ad_x)
     for i in range(len(xs)):
         xs[i] = ad_x[i]
     ys = fun(xs)
-    ad_y = CppADVector(list(ys))
-    f = CppADFunction(ad_x, ad_y)
-    return f
+    ad_y = Vector(list(ys))
+    if mode == Mode.NUMERICAL:
+        raise NotImplementedError("finite diff functor not yet implemented")
+    if mode == Mode.CPPAD:
+        return CppADFunction(ad_x, ad_y)
+    return CGFunction(ad_x, ad_y)
+
+
+class Generated:
+    def __init__(self, function: Callable[[list], list], name: str,
+                 mode: Mode = Mode.CPPAD):
+        self.function = function
+        self.name = name
+        self.mode = mode
+        self.__global_input_dim = 0
+        self.__local_input_dim = -1
+        self.__output_dim = -1
+        self.__is_compiled = False
+
+    def __call__(self, x: list) -> list:
+        return self.function(x)
+
+    def jacobian(self, x: list) -> list:
+        pass
+
+
+    @property
+    def mode(self):
+        return self.mode
+
+    @mode.setter
+    def mode(self, m):
+        if m == self.mode:
+            return
+        print("Setting mode to", m)
+        self.mode = m
+
+    @property
+    def input_dim(self):
+        return self.__global_input_dim + self.__local_input_dim
+
+    @property
+    def output_dim(self):
+        return self.__output_dim
+
+    def discard_library(self):
+        pass
+
+    @property
+    def is_compiled(self):
+        return self.__is_compiled
+
+
+
+# def trace(fun, xs) -> CppADFunction:
+#     ad_x = CppADVector([CppADScalar(x) for x in xs])
+#     independent(ad_x)
+#     for i in range(len(xs)):
+#         xs[i] = ad_x[i]
+#     ys = fun(xs)
+#     ad_y = CppADVector(list(ys))
+#     f = CppADFunction(ad_x, ad_y)
+#     return f
+
+
+# def trace_cg(fun, xs) -> CGFunction:
+#     ad_x = CGVector([CGScalar(x) for x in xs])
+#     independent(ad_x)
+#     for i in range(len(xs)):
+#         xs[i] = ad_x[i]
+#     ys = fun(xs)
+#     ad_y = CGVector(list(ys))
+#     f = CGFunction(ad_x, ad_y)
+#     return f
--- python/tests/test_adfun.py
+++ python/tests/test_adfun.py
@@ -1,39 +1,45 @@
 import numpy as np
 import autogen as ag
-from _autogen import *
 
 def test_function(in_x):
-  out_y = np.ones(1, dtype=ag.CppADScalar)
+  out_y = np.ones(1, dtype=ag.scalar_type())
   for i in range(len(in_x)):
     out_y[0] *= in_x[i] ** 2.
 
   return out_y
 
-def test_function_CG(in_x):
-  out_y = np.ones(1, dtype=ag.CGScalar)
-  for i in range(len(in_x)):
-    out_y[0] *= in_x[i] ** 2.
+# def test_function(in_x):
+#   out_y = np.ones(1, dtype=ag.CppADScalar)
+#   for i in range(len(in_x)):
+#     out_y[0] *= in_x[i] ** 2.
 
-  return out_y
+#   return out_y
+
+# def test_function_CG(in_x):
+#   out_y = np.ones(1, dtype=ag.CGScalar)
+#   for i in range(len(in_x)):
+#     out_y[0] *= in_x[i] ** 2.
+
+#   return out_y
 
 
-# f = ag.trace(test_function, [1., 2.])
-# print(f.to_json())
-#
-# x = [2.0, 3.0]
-# y = f.forward(x)
-# print("y = ", y)
-# J = f.jacobian(x)
-# print("j = ", J)
+f = ag.trace(test_function, [1., 2.], mode=ag.Mode.CPU)
+print(f.to_json())
 
-xs = [1., 2.]
-ad_x = CGVector([CGScalar(x) for x in xs])
-independent(ad_x)
-for i in range(len(xs)):
-  xs[i] = ad_x[i]
-ys = test_function_CG(xs)
-ad_y = CGVector(list(ys))
-f = CGFunction(ad_x, ad_y)
-print(type(f))
-gen = Autogen("test", f)
+x = [2.0, 3.0]
+y = f.forward(x)
+print("y = ", y)
+J = f.jacobian(x)
+print("j = ", J)
+
+# xs = [1., 2.]
+# ad_x = ag.CGVector([ag.CGScalar(x) for x in xs])
+# ag.independent(ad_x)
+# for i in range(len(xs)):
+#   xs[i] = ad_x[i]
+# ys = test_function_CG(xs)
+# ad_y = ag.CGVector(list(ys))
+# f = ag.CGFunction(ad_x, ad_y)
+# print(type(f))
+# gen = ag.Autogen("test", f)
 
