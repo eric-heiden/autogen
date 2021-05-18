06961781f389bc313f3595a96ab63bdf570f061c
Eric Heiden <eric-heiden@users.noreply.github.com>
2021-05-18 14:01:33 -0700

    Improve debugging on Windows

--- include/autogen/cuda/cuda_library.hpp
+++ include/autogen/cuda/cuda_library.hpp
@@ -4,21 +4,53 @@
 
 #if CPPAD_CG_SYSTEM_WIN
 #include <windows.h>
+
+namespace {
+static std::string GetLastErrorAsString() {
+  // https://stackoverflow.com/a/17387176
+  // Get the error message ID, if any.
+  DWORD errorMessageID = ::GetLastError();
+  if (errorMessageID == 0) {
+    return std::string();  // No error message has been recorded
+  }
+
+  LPSTR messageBuffer = nullptr;
+
+  // Ask Win32 to give us the string version of that message ID.
+  // The parameters we pass in, tell Win32 to create the buffer that holds the
+  // message for us (because we don't yet know how long the message string will
+  // be).
+  size_t size = FormatMessageA(
+      FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
+          FORMAT_MESSAGE_IGNORE_INSERTS,
+      NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      (LPSTR)&messageBuffer, 0, NULL);
+
+  // Copy the error message into a std::string.
+  std::string message(messageBuffer, size);
+
+  // Free the Win32's string's buffer.
+  LocalFree(messageBuffer);
+
+  return message;
+}
+}  // namespace
 #endif
 
-#include "cuda_model.hpp"
 #include "cuda_library_processor.hpp"
+#include "cuda_model.hpp"
 
 namespace autogen {
-template <typename Scalar> class CudaLibrary {
-protected:
+template <typename Scalar>
+class CudaLibrary {
+ protected:
   using ModelInfoFunctionPtr = void (*)(char const *const **names, int *count);
 
   void *lib_handle_{nullptr};
 
   std::map<std::string, CudaModel<Scalar>> models_;
 
-public:
+ public:
   /**
    * Opens the dynamic library with the given basename (filename without
    * extension), and loads the CUDA models.
@@ -32,8 +64,9 @@ public:
     lib_handle_ = LoadLibrary(abs_path.c_str());
     if (lib_handle_ == nullptr) {
       throw std::runtime_error("Failed to dynamically load library '" +
-                               library_basename + "': error code " +
-                               std::to_string(GetLastError()));
+                               library_basename + "' (error code " +
+                               std::to_string(GetLastError()) +
+                               "): " + GetLastErrorAsString());
     }
 #else
   CudaLibrary(const std::string &library_basename, std::string path = "",
@@ -57,13 +90,11 @@ public:
     int count;
     model_info_fun(&names, &count);
     std::cout << "Found " << count << " model";
-    if (count != 1)
-      std::cout << "s";
+    if (count != 1) std::cout << "s";
     std::cout << ": ";
     for (int i = 0; i < count; ++i) {
       std::cout << names[i];
-      if (i < count - 1)
-        std::cout << ", ";
+      if (i < count - 1) std::cout << ", ";
       models_.emplace(std::make_pair(std::string(names[i]),
                                      CudaModel<Scalar>(names[i], lib_handle_)));
     }
@@ -85,4 +116,4 @@ public:
     return names;
   }
 };
-} // namespace autogen
+}  // namespace autogen
